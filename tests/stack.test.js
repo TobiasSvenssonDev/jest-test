e6c11db2982cf1b2ed0a5e9080614213e558b3f1
TobiasSvenssonDev <81237750+TobiasSvenssonDev@users.noreply.github.com>
2022-05-06 08:02:43 +0200

    FIXED: The order of the tests made the last test recieve 3 instead of 2

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -4,20 +4,18 @@ test('peek on empty stack returns undefined', () => {
     expect(stack.peek()).toBeUndefined();
 });
 
-test('peek on stack with one element returns that element', () => {
-    stack.push(1);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(1);
-});
-
 test('stack with two elements returns a length of 2', () => {
-    while (stack.length>0){
-        stack.stackLength.pop();
-    };
+
     stack.push("a");
     stack.push("b");
     expect(stack.stackLength().length).toBeDefined();  
     expect(stack.stackLength().length).toBe(2);  
+});
+
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
 });
 
 test('peek on stack with two or more elements returns the top element', () => {
